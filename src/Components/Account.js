f77725a15cc2c63e472a843917adb0eee97692b2
developerYeasin <yea13sin@gmail.com>
2022-05-05 23:13:28 +0600

    alsdjf

---
+++ src/Components/Account.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Section/Footer';
+import Navbar from './Section/Navbar';
+
+const Account = () => {
+    return (
+      <div className="acoount">
+          <Navbar status={""}/>
+          <Footer />
+      </div>
+    );
+};
+
+export default Account;
